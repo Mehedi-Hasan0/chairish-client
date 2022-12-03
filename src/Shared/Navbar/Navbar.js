41bd7546ed91864a57ffdb93642c47e2bc2234d8
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-12-03 16:03:10 +0600

    navbar added

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import user from '../../assets/user.svg';
+import cart from '../../assets/cart.svg';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>This is Navbar</h2>
-        </div>
+        <nav className=' bg-primary p-5 text-white'>
+            <div className=' flex justify-around items-center'>
+                <Link className='text-3xl font-semibold' to='/'>Chairish</Link>
+                <div className='nav_link'>
+                    <Link className='text-sm' to='/'>Home</Link>
+                    <Link className='text-sm' to='/shop'>Shop</Link>
+                    <Link className='text-sm' to='/aboutus'>About Us</Link>
+                    <Link className='text-sm' to='/services'>Services</Link>
+                    <Link className='text-sm' to='/blog'>Blog</Link>
+                    <Link className='text-sm' to='/contactus'>Contact Us</Link>
+                </div>
+                <div className='flex w-16 justify-between'>
+                    <img src={user} alt="" />
+                    <img src={cart} alt="" />
+                </div>
+            </div>
+        </nav>
     );
 };
 
